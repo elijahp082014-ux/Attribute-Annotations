92f32971fb31d01eb2e8618c61ab7d51b40cd900
elijahp082014-ux <elijahp082014@gmail.com>
2026-03-09 18:14:51 -0400

    Attribute Annotations

--- src/main/java/io/elijahsonicboy/annotator/AnnotationProcessor.java
+++ src/main/java/io/elijahsonicboy/annotator/AnnotationProcessor.java
@@ -1,9 +1,28 @@
 package io.elijahsonicboy.annotator;
 
+import io.elijahsonicboy.annotator.annotations.ItemAttribute;
+
+import java.lang.reflect.Field;
+
 public class AnnotationProcessor {
 
-    public static void process() {
-        System.out.println("Annotator is scanning for annotations...");
+    public static void process(Class<?> clazz) {
+        System.out.println("Scanning " + clazz.getName());
+
+        for (Field field : clazz.getDeclaredFields()) {
+
+            if (field.isAnnotationPresent(ItemAttribute.class)) {
+
+                ItemAttribute attr = field.getAnnotation(ItemAttribute.class);
+
+                System.out.println(
+                        "Found attribute: " +
+                                attr.type() +
+                                " value=" +
+                                attr.value()
+                );
+            }
+        }
     }
 
 }
